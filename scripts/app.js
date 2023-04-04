1ee1eeed27b8576f54aaf9bdb080e96f96813dbc
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2023-04-04 21:41:45 +0200

    create fighters

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,38 @@
-const fighters =[
-    {
-        name: "Wizard",
-        shortDesc:"Lorem ipsum",
-        attack:8,
-        bigAttack:12,
-        defence:5,
-        bestSkill: 5,
-        health:100,
-        potions:3,
-        avatar:""
-    },
-    {
-        name: "Soldier",
-        shortDesc:"Lorem ipsum",
-        attack:10,
-        bigAttack:15,
-        defence:3,
-        bestSkill: 5,
-        health:100,
-        potions:3,
-        avatar:""
-    },  
-    {
-        name: "Hunter",
-        shortDesc:"Lorem ipsum",
-        attack:11,
-        bigAttack:13,
-        defence:10,
-        bestSkill: 5,
-        health:100,
-        potions:3,
-        avatar:""
-    },
-]
+import { renderFighterList } from "./renderFighters.js";
+let fighters = [
+  {
+    name: "Wizard",
+    shortDesc: "Lorem ipsum",
+    attack: 8,
+    specialAttack: 12,
+    defence: 5,
+    criticAttack: { name: "Czar złości", value: 20 },
+    health: 100,
+    potions: 3,
+    avatar: "./images/wizard-g090ae14b0_640.png",
+  },
+  {
+    name: "Soldier",
+    shortDesc: "Lorem ipsum",
+    attack: 10,
+    specialAttack: 12,
+    defence: 3,
+    criticAttack: { name: "Headshot", value: 20 },
+    health: 100,
+    potions: 3,
+    avatar: "./images/army-g76cac165d_640.png",
+  },
+  {
+    name: "Hunter",
+    shortDesc: "Lorem ipsum",
+    attack: 11,
+    specialAttack: 12,
+    defence: 10,
+    criticAttack: { name: "Zatruta strzała", value: 20 },
+    health: 100,
+    potions: 3,
+    avatar: "./images/hunter-gc9223ba2f_640.png",
+  },
+];
 
-console.log(fighters);
+renderFighterList(fighters);
