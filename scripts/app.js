841a3d80711dc06c89e0696826cd1df19a9d93cb
Łukasz Bajor <LBajor@opteam.pl>
2023-04-04 14:55:34 +0200

    fighters array and json first step

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+const fighters =[
+    {
+        name: "Wizard",
+        shortDesc:"Lorem ipsum",
+        attack:8,
+        bigAttack:12,
+        defence:5,
+        bestSkill: 5,
+        health:100,
+        potions:3,
+        avatar:""
+    },
+    {
+        name: "Soldier",
+        shortDesc:"Lorem ipsum",
+        attack:10,
+        bigAttack:15,
+        defence:3,
+        bestSkill: 5,
+        health:100,
+        potions:3,
+        avatar:""
+    },  
+    {
+        name: "Hunter",
+        shortDesc:"Lorem ipsum",
+        attack:11,
+        bigAttack:13,
+        defence:10,
+        bestSkill: 5,
+        health:100,
+        potions:3,
+        avatar:""
+    },
+]
+
+console.log(fighters);
